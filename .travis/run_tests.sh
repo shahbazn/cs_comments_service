5163572b747da79a0a90916d1b6a717c06a85eb1
Robert Raposa <rraposa@edx.org>
2016-12-05 16:53:01 -0500

    Output gems for debugging.

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -4,7 +4,11 @@ export MONGOHQ_URL="mongodb://mongo.edx:27017/cs_comments_service_test"
 
 cd /edx/app/forum/cs_comments_service
 
+ls -al /edx/app/forum/.gem/ruby/1.9.1/gems
+
 bundle install
+
+ls -al /edx/app/forum/.gem/ruby/1.9.1/gems
 
 bin/rake search:initialize
 
